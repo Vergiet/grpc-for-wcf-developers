7c98715feb0ae4df8d45d979b88ef2412df345c8
djhhoornweg@hotmail.com <djhhoornweg@hotmail.com>
2021-01-24 15:05:42 +0100

    asd

--- buildimages.ps1
+++ buildimages.ps1
@@ -1,2 +1,2 @@
-docker build -t stockdata:1.0.0 -f .\src\StockData\Dockerfile .
-docker build -t stockweb:1.0.0 -f .\src\StockWeb\Dockerfile .
+docker build -t stockdata:1.0.0 -f .\src\stockdata\Dockerfile ./src/stockdata
+docker build -t stockweb:1.0.0 -f .\src\stockweb\Dockerfile ./src/stockweb
